db501b11e8b426e13b30cc044d486cadd88aca7a
zaizaier <rylec23@gmail.com>
2020-11-04 21:20:10 +0100

    paginacja działa zostalo obramowanie

--- give-out-stuff/src/components/HomeHelpWho.js
+++ give-out-stuff/src/components/HomeHelpWho.js
@@ -1,11 +1,15 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState } from "react";
 
-const HomeHelp = () => {
+const HomeHelpWho = () => {
   const [clientList, setClientList] = useState("");
   const [description, setDescription] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const [perPage, setPerPage] = useState(3);
   const [pages, setPages] = useState([]);
+
+  const [activeF, setActiveF] = useState(false);
+  const [activeO, setActiveO] = useState(false);
+  const [activeZ, setActiveZ] = useState(false);
 
   const API = "http://localhost:8000";
 
@@ -22,7 +26,7 @@ const HomeHelp = () => {
     ? currentOnPage.map((element, index) => {
         return (
           <li key={index}>
-            {element.name} {element.items}
+            <h1>{element.name}</h1> <h2>{element.items}</h2>
           </li>
         );
       })
@@ -51,6 +55,9 @@ const HomeHelp = () => {
 
   const handleFoundations = () => {
     setPages([]);
+    setActiveF((prev) => !prev);
+    setActiveO(false);
+    setActiveZ(false);
     fetch(`${API}/fundacje`)
       .then((response) => response.json())
       .then((data) => {
@@ -64,6 +71,9 @@ const HomeHelp = () => {
   };
 
   const handleOrganizations = () => {
+    setActiveO((prev) => !prev);
+    setActiveZ(false);
+    setActiveF(false);
     setPages([]);
     fetch(`${API}/organizacje`)
       .then((response) => response.json())
@@ -79,7 +89,12 @@ const HomeHelp = () => {
   };
 
   const handleCollections = () => {
+    setActiveZ((prev) => !prev);
+    setActiveO(false);
+    setActiveF(false);
+    console.log(clientList);
     setPages([]);
+    setCurrentPage(1);
     fetch(`${API}/zbiorki`)
       .then((response) => response.json())
       .then((data) => {
@@ -92,6 +107,7 @@ const HomeHelp = () => {
   };
 
   useEffect(() => {
+    setActiveO(true);
     fetch(`${API}/organizacje`)
       .then((response) => response.json())
       .then((data) => {
@@ -109,86 +125,40 @@ const HomeHelp = () => {
       <h1 className="helpWho__heading">Komu pomagamy?</h1>
       <div className="helpWho__decor"></div>
       <ul className="helpWho__navigation">
-        <li onClick={handleFoundations} className="active_foundations">
+        <li
+          onClick={handleFoundations}
+          className={`helpWho__navigation__active_foundations ${
+            activeF ? "active" : ""
+          } `}
+        >
           Fundacjom
         </li>
-        <li onClick={handleOrganizations} className="active_organizations">
+        <li
+          onClick={handleOrganizations}
+          className={`helpWho__navigation__active_organizations ${
+            activeO ? "active" : ""
+          } `}
+        >
           Organizacjom pozrządowym
         </li>
-        <li onClick={handleCollections} className="active_collections">
+        <li
+          onClick={handleCollections}
+          className={`helpWho__navigation__active_collections ${
+            activeZ ? "active" : ""
+          } `}
+        >
           Lokalnym zbiórkom
         </li>
       </ul>
       <div className="helpWho__content">
         <div className="helpWho__content__description">{description}</div>
-        <ul>{clientList && renderList}</ul>
-        <ul>{pages.length > 0 && clientList ? renderPageNumbers : ""}</ul>
+        <ul className="helpWho__content__list">{clientList && renderList}</ul>
+        <ul className="helpWho__content__pagination">
+          {pages.length > 0 && clientList ? renderPageNumbers : ""}
+        </ul>
       </div>
     </section>
   );
 };
 
-export default HomeHelp;
-// Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br></br>{" "}
-// do eiusmod tempor incididunt ut labore et dolore magna <br></br>{" "}
-// aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
-
-// class TodoApp extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       todos: ['a','b','c','d','e','f','g','h','i','j','k'],
-//       currentPage: 1,
-//       todosPerPage: 3
-//     };
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-
-// handleClick(event) {
-//   this.setState({
-//     currentPage: Number(event.target.id)
-//   });
-// }
-
-//   render() {
-//     const { todos, currentPage, todosPerPage } = this.state;
-
-//     // Logic for displaying current todos
-//     const indexOfLastTodo = currentPage * todosPerPage;
-//     const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
-//     const currentTodos = todos.slice(indexOfFirstTodo, indexOfLastTodo);
-
-//     const renderTodos = currentTodos.map((todo, index) => {
-//       return <li key={index}>{todo}</li>;
-//     });
-
-//     // Logic for displaying page numbers
-//     const pageNumbers = [];
-//     for (let i = 1; i <= Math.ceil(todos.length / todosPerPage); i++) {
-//       pageNumbers.push(i);
-//     }
-
-//     const renderPageNumbers = pageNumbers.map(number => {
-//       return (
-//         <li
-//           key={number}
-//           id={number}
-//           onClick={this.handleClick}
-//         >
-//           {number}
-//         </li>
-//       );
-//     });
-
-//     return (
-//       <div>
-//         <ul>
-//           {renderTodos}
-//         </ul>
-//         <ul id="page-numbers">
-//           {renderPageNumbers}
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
+export default HomeHelpWho;
