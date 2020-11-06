e00f74e116e8e934a702dc8db02761610fd5cef4
zaizaier <rylec23@gmail.com>
2020-11-06 11:49:47 +0100

    layout zrobiony --poprawic szerokośći

--- give-out-stuff/src/components/Footer.js
+++ give-out-stuff/src/components/Footer.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <footer className="footer">
+      <div className="footer__empty"></div>
+      <div className="footer__copywright">Copyright by Coders Lab</div>
+      <div className="footer__media">
+        <a href="https://www.facebook.com/" target="_blank">
+          <div className="footer__media__fb"></div>
+        </a>
+        <a href="https://www.instagram.com/" target="_blank">
+          <div className="footer__media__ig"></div>
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
